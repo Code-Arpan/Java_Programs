dad83c8f9fbf41a388f7778f432eb1a2aa390c25
Arpan <arpan.pal1966@gmail.com>
2022-03-07 18:02:04 +0530

    Input & find STD codes

---
+++ src/STD_Codes.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class STD_Codes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] cities = new String[3];
+        String[] std = new String[3];
+
+        System.out.println("Enter Cities: ");
+        for(int i=0;i<cities.length;i++){
+            cities[i]= sc.next();
+
+        }
+
+        System.out.println("Enter STD codes: ");
+        for(int i=0;i<std.length;i++){
+            std[i]= sc.next();
+        }
+
+        System.out.println("Enter Name for searching");
+        String target = sc.next();
+
+        for(int i=0;i<3;i++){
+            if(target.equalsIgnoreCase(cities[i])){
+                System.out.println(cities[i]+" : "+std[i]);
+            }
+        }
+    }
+}
