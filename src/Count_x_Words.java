26afdf4b114d7193215b349519dc4cd2b4d93b82
Arpan <arpan.pal1966@gmail.com>
2022-03-18 17:00:05 +0530

    Minor Update

---
+++ src/Count_x_Words.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Count_x_Words {
+    public static void main(String[] args){
+        String sen;
+        int i,c=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input a Sentence: ");
+        sen = sc.nextLine();
+        System.out.print("Input a Character to Count: ");
+        char x = sc.next().charAt(0);
+        sen = sen.toUpperCase();
+        sen = " " + sen;
+
+        for(i=0;i<sen.length();i++){
+            if(sen.charAt(i)==' ' && sen.charAt(i+1)==x){
+                c++;
+            }
+        }
+        System.out.println("Total number of words starting with '"+x+"' = "+c);
+    }
+}
