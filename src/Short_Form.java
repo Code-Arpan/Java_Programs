65169058d2c8a729f7413220b4c21b0999d7bc21
Arpan <arpan.pal1966@gmail.com>
2022-03-19 17:13:31 +0530

    Short form of a sentence

--- src/Short_Form.java
+++ src/Short_Form.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
-public class First_Letter{
+public class Short_Form {
     public static void main(String[] args) {
-        String sen, fls="";
+        String sen, sf="";
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Sentence: ");
+        System.out.println("Enter a Sentence: ");
         sen = sc.nextLine();
         sen = " " + sen;     //ading one space before the sentence
 
         for(int i=0;i<sen.length();i++){
             if(sen.charAt(i) == ' '){
-                fls+=sen.charAt(i+1)+".";  //adding the character after every space with a '.'
+                sf+=sen.charAt(i+1)+".";  //adding the character after every space with a '.'
             }
         }
 
-        fls = fls.toUpperCase();
-        System.out.println("First Letters: "+fls);
+        sf = sf.toUpperCase();
+        System.out.println("First Letters: "+sf);
     }
 }
